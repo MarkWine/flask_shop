b96aa3dabe84f39368f12f2f01b178de9346dd91
mark <marcoxwine@gmail.com>
2019-03-29 16:23:10 -0700

    postgresql support

--- shop/__init__.py
+++ shop/__init__.py
@@ -9,8 +9,8 @@ from sqlalchemy_utils import database_exists, create_database
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("config.py", silent=True)
 
-if not database_exists(app.config.get("SQLALCHEMY_DATABASE_URI")):
-    create_database(app.config.get("SQLALCHEMY_DATABASE_URI"))
+# Sif not database_exists(app.config.get("SQLALCHEMY_DATABASE_URI")):
+#     create_database(app.config.get("SQLALCHEMY_DATABASE_URI"))
 db = SQLAlchemy(app)
 
 from .blueprints.cart.views import cart_blueprint
