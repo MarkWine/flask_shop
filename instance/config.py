82a7a2a7c41661a35258a2031c28dc57486cbecd
mark <marcoxwine@gmail.com>
2019-03-29 15:46:22 -0700

    postgresql support

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,9 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "sqlite:///:memory:"
+SQLALCHEMY_DATABASE_URI = (
+    os.environ.get("SQLALCHEMY_DATABASE_URI")
+    or os.environ.get("DATABASE_URL")  # easy Heroku support
+    or "sqlite:///:memory:"
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "howdy"
